63cc4907dc32c2c04f3acbf229b01ebbfaad4ada
Faisal <53616522+faisalfarooqqq@users.noreply.github.com>
2024-04-15 13:36:05 +0500

    updated error handling

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -26,6 +26,25 @@ const getWorkout = async (req, res) => {
 // create a workout
 const createWorkout = async (req, res) => {
     const {title, load, reps} = req.body;
+
+    let emptyFields = [];
+
+    if(!title) {
+        emptyFields.push('title');
+    }
+
+    if(!load) {
+        emptyFields.push('load');
+    }
+
+    if(!reps) {
+        emptyFields.push('reps');
+    }
+
+    if(emptyFields.length>0){
+        return res.status(400).json({error: "Please fill in the required fields", emptyFields});
+    }
+    
     //add doc to db
     try {
         const user_id = req.user._id;
